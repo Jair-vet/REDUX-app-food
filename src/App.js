3a371ae07560af95a338b0ae54c0bf3f50c01b05
Jair <jairaceves56@gmail.com>
2023-11-09 15:00:02 -0600

    Creamos el Router dinamico con react

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import { Header } from "./components/Header";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Products } from "./components/Products";
+import { NuevoProducto } from "./components/NuevoProducto";
 
 function App() {
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <Header />
+
+        <div className="container">
+          <Routes>
+            <Route index element={<Products />} />
+            <Route path="/nuevo-producto" element={<NuevoProducto/>}/>
+          </Routes >
+        </div>
+
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom"
 
 export const Header = () => {
   return (
@@ -8,10 +9,10 @@ export const Header = () => {
             </div>
 
             <div className="md:flex md:w-1/3 md:justify-end md:mt-0 mt-3">
-                <a 
+                <Link 
                     className="btn btn-danger nuevo-post d-block d-md-inline-block"
-                    href="/productos/nuevo">Agregar Producto &#43;
-                </a>
+                    to="nuevo-producto">Agregar Producto &#43;
+                </Link>
             </div>
         </div>
     </nav>
