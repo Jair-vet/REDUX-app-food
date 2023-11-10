b52092eb9af997a0822f70e2290ca6c3283ec1af
Jair <jairaceves56@gmail.com>
2023-11-09 18:35:45 -0600

    Creamos la tabla de Productos

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,8 +1,22 @@
+import productos from '../data/db.json'
 
 export const Products = () => {
     return (
       <>
-        <h1>Aqui estan los Productos</h1>
+        <h2 className="text-center text-3xl font-bold p-4">Listado de Productos</h2>
+        <div className='m-3'>
+          <table className='w-full rounded-md'>
+            <thead className="bg-emerald-600">
+              <tr className='rounded-md'>
+                <th className='text-white text-center p-2'>Nombre</th>
+                <th className='text-white text-center p-2'>Precio</th>
+                <th className='text-white text-center p-2 rounded-r-md'>Acciones</th>
+              </tr>
+            </thead>
+            <tbody>
+            </tbody>
+          </table>
+        </div>
       </>
     )
   }
